2e0bad0f1720facf228847498da3f1c360e27562
mahuwei <mahuwei@msn.com>
2018-06-11 10:46:19 +0800

    参见“压缩.md”。

---
+++ core2.1.test/BrotliCompressionProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace core2._1.test {
+    public class BrotliCompressionProvider : ICompressionProvider {
+        public string EncodingName => "br";
+        public bool SupportsFlush => true;
+
+        public Stream CreateStream(Stream outputStream) {
+            return new BrotliStream(outputStream, CompressionLevel.Fastest);
+        }
+    }
+}
---
+++ core2.1.test/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace core2._1.test {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddResponseCompression(options => {
+                options.Providers.Add<BrotliCompressionProvider>();
+                // 现在设置了两种需求，以后开需要在添加
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {"image/svg+xml","application/json"});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseResponseCompression();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
